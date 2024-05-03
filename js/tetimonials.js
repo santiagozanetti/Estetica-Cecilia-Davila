3b7070b2cb0a663939c0c66a3ae33ffb280749e2
Santiago <santizanetti117@gmail.com>
2024-05-03 18:01:06 -0300

    seccion testimonios

---
+++ js/tetimonials.js
@@ -0,0 +1,13 @@
+// const swiper = new Swiper(".js-testimonials-slider", {
+//   grabCursor: true,
+//   spaceBetween: 30,
+//   pagination: {
+//     el: ".js-testimonials-pagination",
+//     clickable: true,
+//   },
+//   breakpoints: {
+//     767: {
+//       slidePerView: 2,
+//     },
+//   },
+// });
