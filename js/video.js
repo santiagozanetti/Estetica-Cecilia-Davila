17ba6766b20b21519e4ef78459ce491486a51898
Santiago <santizanetti117@gmail.com>
2024-05-06 23:53:53 -0300

    agrego seccion promociones, contacto y footer. Videos se reproducen cada 8 segundos. Falta acomodar seccion testimonios

--- js/video.js
+++ js/video.js
@@ -1,27 +1,54 @@
-const btns = document.querySelectorAll(".nav-btn");
-const slides = document.querySelectorAll(".video-slide");
-const contents = document.querySelectorAll(".content");
+document.addEventListener("DOMContentLoaded", function () {
+  const btns = document.querySelectorAll(".nav-btn");
+  const slides = document.querySelectorAll(".video-slide");
+  const contents = document.querySelectorAll(".content");
 
-var sliderNav = function (manual) {
-  btns.forEach((btn) => {
-    btn.classList.remove("active");
-  });
+  let currentSlide = 0;
 
-  slides.forEach((slide) => {
-    slide.classList.remove("active");
-  });
+  function nextSlide() {
+    btns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
 
-  contents.forEach((content) => {
-    content.classList.remove("active");
-  });
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
 
-  btns[manual].classList.add("active");
-  slides[manual].classList.add("active");
-  contents[manual].classList.add("active");
-};
+    contents.forEach((content) => {
+      content.classList.remove("active");
+    });
 
-btns.forEach((btn, i) => {
-  btn.addEventListener("click", () => {
-    sliderNav(i);
+    currentSlide++;
+    if (currentSlide >= slides.length) {
+      currentSlide = 0;
+    }
+
+    btns[currentSlide].classList.add("active");
+    slides[currentSlide].classList.add("active");
+    contents[currentSlide].classList.add("active");
+  }
+
+  function autoPlay() {
+    setInterval(nextSlide, 8000);
+  }
+
+  autoPlay();
+
+  btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+      currentSlide = i;
+      btns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      slides.forEach((slide) => {
+        slide.classList.remove("active");
+      });
+      contents.forEach((content) => {
+        content.classList.remove("active");
+      });
+      btns[currentSlide].classList.add("active");
+      slides[currentSlide].classList.add("active");
+      contents[currentSlide].classList.add("active");
+    });
   });
 });
