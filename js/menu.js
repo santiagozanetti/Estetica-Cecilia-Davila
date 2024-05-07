871afa6332877ad2371ee9192e8dea6766e5bdb3
Santiago <santizanetti117@gmail.com>
2024-05-07 20:49:38 -0300

    agrego botones flotantes, modifico botones para celulares y acomodo footer

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,17 @@
 const menuBtn = document.querySelector(".menu-btn");
 const navigation = document.querySelector(".navigation");
+const navigationItems = document.querySelectorAll(".navigation-items a");
 
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("active");
   navigation.classList.toggle("active");
+});
+
+// Agregar evento de clic a cada enlace del menú
+navigationItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    // Cerrar el menú al hacer clic en un enlace
+    menuBtn.classList.remove("active");
+    navigation.classList.remove("active");
+  });
 });
