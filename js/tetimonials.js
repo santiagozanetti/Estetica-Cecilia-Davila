d22f7b7421029686c65e301f637011901a43e9af
Santiago <santizanetti117@gmail.com>
2024-05-08 20:38:11 -0300

    seccion testimonios para escritorio, falta responsive

--- js/tetimonials.js
+++ js/tetimonials.js
@@ -1,13 +1,15 @@
-// const swiper = new Swiper(".js-testimonials-slider", {
-//   grabCursor: true,
-//   spaceBetween: 30,
-//   pagination: {
-//     el: ".js-testimonials-pagination",
-//     clickable: true,
-//   },
-//   breakpoints: {
-//     767: {
-//       slidePerView: 2,
-//     },
-//   },
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  // Inicializa Swiper
+  var mySwiper = new Swiper(".swiper-container", {
+    slidesPerView: 2, // Mostrar 2 slides al mismo tiempo
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+      delay: 6000, // Cambia cada 5 segundos
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+});
