30e8322a0303b1aac6421b047a34a81741b38557
Santiago <santizanetti117@gmail.com>
2024-08-06 19:59:10 -0300

    footer mejorado

--- js/faq.js
+++ js/faq.js
@@ -1,114 +1,114 @@
-class Accordion {
-  constructor(el) {
-    this.el = el;
-    this.summary = el.querySelector("summary");
-    this.content = el.querySelector(".faq-content");
-    this.expandIcon = this.summary.querySelector(".expand-icon");
-    this.animation = null;
-    this.isClosing = false;
-    this.isExpanding = false;
-    this.summary.addEventListener("click", (e) => this.onClick(e));
-  }
+// class Accordion {
+//   constructor(el) {
+//     this.el = el;
+//     this.summary = el.querySelector("summary");
+//     this.content = el.querySelector(".faq-content");
+//     this.expandIcon = this.summary.querySelector(".expand-icon");
+//     this.animation = null;
+//     this.isClosing = false;
+//     this.isExpanding = false;
+//     this.summary.addEventListener("click", (e) => this.onClick(e));
+//   }
 
-  onClick(e) {
-    e.preventDefault();
-    if (!this.el.open) {
-      this.closeAll();
-      this.open();
-    } else {
-      this.shrink();
-    }
-  }
+//   onClick(e) {
+//     e.preventDefault();
+//     if (!this.el.open) {
+//       this.closeAll();
+//       this.open();
+//     } else {
+//       this.shrink();
+//     }
+//   }
 
-  closeAll() {
-    document.querySelectorAll("details").forEach((detail) => {
-      if (detail !== this.el && detail.open) {
-        detail.open = false;
-        detail
-          .querySelector(".expand-icon")
-          .setAttribute("src", "assets/plus.svg");
-      }
-    });
-  }
+//   closeAll() {
+//     document.querySelectorAll("details").forEach((detail) => {
+//       if (detail !== this.el && detail.open) {
+//         detail.open = false;
+//         detail
+//           .querySelector(".expand-icon")
+//           .setAttribute("src", "assets/plus.svg");
+//       }
+//     });
+//   }
 
-  shrink() {
-    this.isClosing = true;
-    const startHeight = `${this.el.offsetHeight}px`;
-    const endHeight = `${this.summary.offsetHeight}px`;
+//   shrink() {
+//     this.isClosing = true;
+//     const startHeight = `${this.el.offsetHeight}px`;
+//     const endHeight = `${this.summary.offsetHeight}px`;
 
-    if (this.animation) {
-      this.animation.cancel();
-    }
+//     if (this.animation) {
+//       this.animation.cancel();
+//     }
 
-    this.animation = this.el.animate(
-      {
-        height: [startHeight, endHeight],
-      },
-      {
-        duration: 400,
-        easing: "ease-out",
-      }
-    );
+//     this.animation = this.el.animate(
+//       {
+//         height: [startHeight, endHeight],
+//       },
+//       {
+//         duration: 400,
+//         easing: "ease-out",
+//       }
+//     );
 
-    this.animation.onfinish = () => {
-      this.expandIcon.setAttribute("src", "assets/plus.svg");
-      return this.onAnimationFinish(false);
-    };
-    this.animation.oncancel = () => {
-      this.expandIcon.setAttribute("src", "assets/plus.svg");
-      return (this.isClosing = false);
-    };
-  }
+//     this.animation.onfinish = () => {
+//       this.expandIcon.setAttribute("src", "assets/plus.svg");
+//       return this.onAnimationFinish(false);
+//     };
+//     this.animation.oncancel = () => {
+//       this.expandIcon.setAttribute("src", "assets/plus.svg");
+//       return (this.isClosing = false);
+//     };
+//   }
 
-  open() {
-    this.el.style.height = `${this.el.offsetHeight}px`;
-    this.el.open = true;
-    window.requestAnimationFrame(() => this.expand());
-  }
+//   open() {
+//     this.el.style.height = `${this.el.offsetHeight}px`;
+//     this.el.open = true;
+//     window.requestAnimationFrame(() => this.expand());
+//   }
 
-  expand() {
-    this.isExpanding = true;
-    const startHeight = `${this.el.offsetHeight}px`;
-    const endHeight = `${
-      this.summary.offsetHeight + this.content.offsetHeight
-    }px`;
+//   expand() {
+//     this.isExpanding = true;
+//     const startHeight = `${this.el.offsetHeight}px`;
+//     const endHeight = `${
+//       this.summary.offsetHeight + this.content.offsetHeight
+//     }px`;
 
-    if (this.animation) {
-      this.animation.cancel();
-    }
+//     if (this.animation) {
+//       this.animation.cancel();
+//     }
 
-    this.animation = this.el.animate(
-      {
-        height: [startHeight, endHeight],
-      },
-      {
-        duration: 450,
-        easing: "ease-out",
-      }
-    );
+//     this.animation = this.el.animate(
+//       {
+//         height: [startHeight, endHeight],
+//       },
+//       {
+//         duration: 450,
+//         easing: "ease-out",
+//       }
+//     );
 
-    this.animation.onfinish = () => {
-      this.expandIcon.setAttribute("src", "assets/minus.svg");
-      return this.onAnimationFinish(true);
-    };
-    this.animation.oncancel = () => {
-      this.expandIcon.setAttribute("src", "assets/minus.svg");
-      return (this.isExpanding = false);
-    };
-  }
+//     this.animation.onfinish = () => {
+//       this.expandIcon.setAttribute("src", "assets/minus.svg");
+//       return this.onAnimationFinish(true);
+//     };
+//     this.animation.oncancel = () => {
+//       this.expandIcon.setAttribute("src", "assets/minus.svg");
+//       return (this.isExpanding = false);
+//     };
+//   }
 
-  onAnimationFinish(open) {
-    this.el.open = open;
-    this.animation = null;
-    this.isClosing = false;
-    this.isExpanding = false;
-    this.el.style.height = this.el.style.overflow = "";
-  }
-}
+//   onAnimationFinish(open) {
+//     this.el.open = open;
+//     this.animation = null;
+//     this.isClosing = false;
+//     this.isExpanding = false;
+//     this.el.style.height = this.el.style.overflow = "";
+//   }
+// }
 
-document.querySelectorAll("details").forEach((el) => {
-  new Accordion(el);
-});
+// document.querySelectorAll("details").forEach((el) => {
+//   new Accordion(el);
+// });
 
 // --------------- Collapsible
 var coll = document.getElementsByClassName("collapsible");
